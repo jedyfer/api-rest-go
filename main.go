e6ea013f49e7303c8a8180b0c2c0616d6ec7753a
Jediael Fernández <jedyfer21@gmail.com>
2022-10-06 12:03:44 -0500

    4.3 Cors (prueba en console web)

--- main.go
+++ main.go
@@ -11,6 +11,15 @@ func main() {
 
 	//	e.GET("/", holaMundo)
 
+	//	CORS
+	//	DESMARCAR ESTO PARA PROBAR CORS
+	//	e.Use(middleware.CORS()) //	esto permite a cualquier dominio realizar consultas (esto solo es para pruebas)
+	//	IMPORTANTE: se debe especifica que dominios puede realizar consultas
+
+	//	como probarlo
+	//	en el console de la web
+	//	fetch('http://localhost:8080/api/v1/users').the(r => r.json()).then(data => console.log(data));
+
 	startRoutes(e) //	llamando a la funcion startRoutes de route.go
 
 	err := e.Start(":8080") //	localhost:8080
